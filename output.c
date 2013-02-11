2f9d1d6c68a529f0018aad22b791c8200269214f
David Oguns <david.oguns@six3systems.com>
2013-02-11 13:57:29 -0500

    Updated netpbm output.c in pam_output subdirectory. Re-added GL and GLUT libraries linkage in makefile. Verified through Fedora 18 install after installing MesaGL and freeglut.

--- output.c
+++ output.c
@@ -9,63 +9,89 @@
  */
 
 #include <stdlib.h>
-#include <netpbm/pam.h>
-#include <netpbm/ppm.h>
-#include <stdio.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 
 #include "output.h"
 
-/* global command line args for PAM */
+/* global command line args for GLUT */
 extern int g_argc;
 extern char **g_argv;
 
-#define COLORVAL_MAX		0xFF
+/* buffer to draw whenever glDisplayFunc callback is invoked */
+unsigned int *draw_buffer = 0;
+unsigned int draw_width = 0;
+unsigned int draw_height = 0;
 
-/* extract relevant color channels from a 32 bit pixel value */
-#define COLOR_R(x)		((x & 0x00FF0000) >> 16)
-#define COLOR_G(x)		((x & 0x0000FF00) >> 8)
-#define COLOR_B(x)		((x & 0x000000FF))
+/* create buffer glDrawPixels can use it's own format */
+void covert_buffer(unsigned int *buffer, unsigned int width, unsigned int height)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	draw_buffer = (unsigned int *)malloc(sizeof(unsigned int)*width*height);
+
+	for(; i < height; ++i)
+	{
+		for(j = 0; j < width; ++j)
+		{	/* reverse row order and byte order */
+			GLubyte *dest = (GLubyte *)&draw_buffer[i * width + j];
+			GLubyte *src = (GLubyte *)&buffer[(height-(i+1)) * width + j];
+			dest[3] = src[3];	/* A = A */
+			dest[0] = src[2];	/* B = R */
+			dest[1] = src[1];	/* G = G */
+			dest[2] = src[0];	/* R = B */
+		}
+	}
+}
+
+/* draw function */
+void render(void)
+{
+	unsigned int i = 0;
+	unsigned int *buffer = draw_buffer;
+	if(draw_buffer)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glMatrixMode(GL_PROJECTION);
+		glOrtho(0, draw_width, 0, draw_height, 0.0f, 10.0f);
+		/* loop necessary to draw right side up */
+		glDrawPixels(draw_width, draw_height, GL_RGBA, GL_UNSIGNED_BYTE, draw_buffer);
+
+		glFlush();
+	}
+}
 
 /* write buffer to file as a 32 bit image */
 int write_image(const char *filename, unsigned int *buffer,
 	unsigned int width, unsigned int height)
 {
-	pixel **output_buffer = 0;
-	pixval max = COLORVAL_MAX;
-	unsigned int i;
-	unsigned int j = 0;
-	FILE *fp = fopen(filename, "w+");
+	/* initialize glut */
+	glutInit(&g_argc, g_argv);
+	/* create window at 100, 100 */
+	glutInitWindowPosition(100, 100);
+	/* create proper window size */
+	glutInitWindowSize(width, height);
+	/* initialize display mode to 32 bit color */
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	/* create window we just initialized */
+	glutCreateWindow("Ray Tracer Output");
 
-	if(!fp)
-	{
-		printf("Could not open file {%s} for writing.\n", filename);
-		return 0;
-	}
+	/* mark which buffer to draw */
+	//draw_buffer = buffer;	
+	draw_width = width;
+	draw_height = height;
+	covert_buffer(buffer, width, height);
 
-	/* initialize PAM with command line args */
-	ppm_init(&g_argc, g_argv);
+	/* indicate what our display function callback is */
+	glutDisplayFunc(render);
 
-	/* reserve space for image */
-	output_buffer = ppm_allocarray(width, height);
+	glClearColor(.5, .5, .5, 1.0);
 
-	for(; j < height; ++j)
-	{
-		for(i = 0; i < width; ++i)
-		{
-			PPM_ASSIGN(output_buffer[j][i],
-				COLOR_R(buffer[i+j*width]),
-				COLOR_G(buffer[i+j*width]),
-				COLOR_B(buffer[i+j*width]));
-		}
-	}
+	/* initialize main loop */
+	glutMainLoop();
 
-	/* write buffer to file */
-	ppm_writeppm(fp, output_buffer, width, height, max, 0);
-
-	/* free space */
-	ppm_freearray(output_buffer, height);
-
-	fclose(fp);
+	free(draw_buffer);
 
 	return 1;
 }
